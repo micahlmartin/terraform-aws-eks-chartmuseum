59021973de07c721a63f4b6b711906613d1f8699
Andre Silva <andre@particle41.com>
2021-08-10 17:28:20 -0300

    remove kubernetes requirement

--- modules/resources/main.tf
+++ modules/resources/main.tf
@@ -12,11 +12,6 @@ terraform {
       version = ">= 3.46.0"
     }
 
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = ">= 2.3.2"
-    }
-
     helm = {
       source  = "hashicorp/helm"
       version = ">= 2.2.0"
