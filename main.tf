20f3d92f0f16f5a8f45340086a7ed801ae546632
Micah Martin <micahlmartin@gmail.com>
2022-08-28 13:29:49 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -21,22 +21,22 @@ module "resources" {
   k8s_namespace             = var.k8s_namespace
   chart_version_chartmuseum = var.chart_version_chartmuseum
 
-  helm_release_name      = var.helm_release_name
-  helm_recreate_pods     = var.helm_recreate_pods
-  helm_atomic            = var.helm_atomic_creation
-  helm_cleanup_on_fail   = var.helm_cleanup_on_fail
-  helm_wait              = var.helm_wait_for_completion
-  helm_wait_for_jobs     = var.helm_wait_for_jobs
-  helm_timeout_seconds           = var.helm_timeout_seconds
-  helm_max_history       = var.helm_max_history
-  helm_verify            = var.helm_verify
-  helm_keyring           = var.helm_keyring
-  helm_reuse_values      = var.helm_reuse_values
-  helm_reset_values      = var.helm_reset_values
-  helm_force_update      = var.helm_force_update
-  helm_replace           = var.helm_replace
-  helm_create_namespace  = var.helm_create_namespace
-  helm_dependency_update = var.helm_dependency_update
-  helm_skip_crds         = var.helm_skip_crds
+  helm_release_name        = var.helm_release_name
+  helm_recreate_pods       = var.helm_recreate_pods
+  helm_atomic              = var.helm_atomic_creation
+  helm_cleanup_on_fail     = var.helm_cleanup_on_fail
+  helm_wait_for_completion = var.helm_wait_for_completion
+  helm_wait_for_jobs       = var.helm_wait_for_jobs
+  helm_timeout_seconds     = var.helm_timeout_seconds
+  helm_max_history         = var.helm_max_history
+  helm_verify              = var.helm_verify
+  helm_keyring             = var.helm_keyring
+  helm_reuse_values        = var.helm_reuse_values
+  helm_reset_values        = var.helm_reset_values
+  helm_force_update        = var.helm_force_update
+  helm_replace             = var.helm_replace
+  helm_create_namespace    = var.helm_create_namespace
+  helm_dependency_update   = var.helm_dependency_update
+  helm_skip_crds           = var.helm_skip_crds
 
 }
